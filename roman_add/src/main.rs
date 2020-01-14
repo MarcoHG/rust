e768bc0a6e76b0fb82d4a27a9ab43d945f707eb9
MarcoHG <marco.henrygin@gmail.com>
2020-01-14 08:33:53 -0800

    roman_add is in green - make it fail! (top)

--- roman_add/src/main.rs
+++ roman_add/src/main.rs
@@ -14,5 +14,32 @@ fn convert(s: &str) ->String {
 }
 
 fn roman_add( lhs: &str, rhs: &str) -> String {
-	(convert(lhs) + &convert(rhs)).replace("IIIII","V").replace("IIII","IV").replace("VV","X")
+	(convert(lhs) + &convert(rhs)).replace("IIIII","V").replace("IIII","IV").replace("VIV","IX").replace("VV","X")
+}
+
+
+#[cfg(test)]
+// cargo test
+mod tests {
+	use super::*;
+	
+	#[test]
+	fn test_add_i_i() {
+		assert_eq!(roman_add("I","I"), "II");
+	}
+
+	#[test]
+	fn test_add_vi_iv() {
+		assert_eq!(roman_add("VI","IV"), "X");
+	}
+
+	#[test]
+	fn test_add_iv_vi() {
+		assert_eq!(roman_add("IV","VI"), "X");
+	}
+
+	#[test]
+	fn test_add_iv_v() {
+		assert_eq!(roman_add("IV","V"), "IX");
+	}
 }
